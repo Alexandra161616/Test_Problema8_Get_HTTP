ec8f26e0fbc6850ab7295654db9091d6e30da489
Alexandra <georgianaalexandrac@gmail.com>
2023-07-07 05:11:44 +0300

    Add project files.

---
+++ Test_Problema8!/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Test_Problema8_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Azure;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(332, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Azure;
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(92, 322);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(604, 366);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Test_Problema8_.Properties.Resources._1111111111;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(884, 494);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 194);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search Button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(285, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(528, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Introdu numele serialului pe care doresti sa il cauti: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Test_Problema8_.Properties.Resources.da1;
+            this.ClientSize = new System.Drawing.Size(1067, 700);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Test_Problema8!/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace Test_Problema8_
+{
+    public partial class Form1 : Form
+    {
+        public List<Serial> seriale;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                var endpoints = new Uri("https://api.tvmaze.com/search/shows?q=ted");
+                var result = client.GetAsync(endpoints).Result;
+                var json = result.Content.ReadAsStringAsync().Result;
+
+
+                seriale = JsonConvert.DeserializeObject<List<Serial>>(json);
+
+                Serial matchingShow = seriale.FirstOrDefault(s => s.Show != null && s.Show.Name != null && s.Show.Name.Contains(textBox1.Text));
+
+
+
+                if (matchingShow != null)
+                {
+                    Show show = matchingShow.Show;
+                    string serializedShow = JsonConvert.SerializeObject(show, Formatting.Indented);
+                    richTextBox1.AppendText(serializedShow + Environment.NewLine);
+                }
+                else
+                {
+                    richTextBox1.AppendText("No matching show found." + Environment.NewLine);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+    public class Serial
+    {
+        public double Score { get; set; }
+        public Show Show { get; set; }
+    }
+
+    public class Show
+    {
+        public int ID { get; set; }
+        public string URL { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Language { get; set; }
+        public List<string> Genres { get; set; }
+        public string Status { get; set; }
+        public int? Runtime { get; set; }
+        public int? AverageRuntime { get; set; }
+        public string Premiered { get; set; }
+        public string Ended { get; set; }
+        public string OfficialSite { get; set; }
+        public Schedule Schedule { get; set; }
+        public Rating Rating { get; set; }
+        public int Weight { get; set; }
+        public Network Network { get; set; }
+        public WebChannel WebChannel { get; set; }
+        public string DVDCountry { get; set; }
+        public Externals Externals { get; set; }
+        public Image Image { get; set; }
+        public string Summary { get; set; }
+        public long Updated { get; set; }
+        public Links Links { get; set; }
+    }
+
+    public class Schedule
+    {
+        public string Time { get; set; }
+        public List<string> Days { get; set; }
+    }
+
+    public class Rating
+    {
+        public double? Average { get; set; }
+    }
+
+    public class Network
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Country Country { get; set; }
+        public string OfficialSite { get; set; }
+    }
+
+    public class WebChannel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public object Country { get; set; }
+        public string OfficialSite { get; set; }
+    }
+
+    public class Externals
+    {
+        public object Tvrage { get; set; }
+        public int? TheTVDB { get; set; }
+        public string IMDB { get; set; }
+    }
+
+    public class Image
+    {
+        public string Medium { get; set; }
+        public string Original { get; set; }
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Timezone { get; set; }
+    }
+
+    public class Links
+    {
+        public Self Self { get; set; }
+        public Previousepisode Previousepisode { get; set; }
+    }
+
+    public class Self
+    {
+        public string Href { get; set; }
+    }
+
+    public class Previousepisode
+    {
+        public string Href { get; set; }
+    }
+
+  
+
+}
